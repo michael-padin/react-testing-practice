8aef66733c8ef6e4909d7dc1b9085b8eb8e496fd
Your Name <padinmichael201@gmail.com>
2022-09-01 12:20:49 +0800

    test: add test on component

--- src/components/Application/Application.test.tsx
+++ src/components/Application/Application.test.tsx
@@ -1,10 +1,95 @@
 import { screen, render } from "@testing-library/react";
 import Application from "./Application";
 
+/**   QUERY METHODS
+ * getByRole() - the role of the element: see options here (https://testing-library.com/docs/queries/byrole)
+ * getLabelText()  - search for matching given text
+ * getByPlaceholderText()  - search element with a placeholder attribute, find one that matches the given text
+ * getByText() = elements that have a text matching the given text
+ * getByDisplayValue() - return the element that has matching value e.i ["input", 'textarea', "select"]
+ * getByAltText() - return the element that given al text e.i ["image", "input", "area"]
+ *
+ * OPTIONS: {
+ * selector: type of element
+ * name: inner text of label
+ * level: level of heading e.i h1, h2
+ *
+ * }
+ * */
+
 describe("Application", () => {
   it("should render", () => {
     render(<Application />);
-    const InputElement = screen.getByRole("textbox");
-    expect(InputElement).toBeInTheDocument();
+
+    // getByRole h1
+    const pageHeading = screen.getByRole("heading", {
+      name: "Job Application Form",
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    // getByRole h2
+    const sectionHeading = screen.getByRole("heading", { name: "Section 1" });
+    expect(sectionHeading).toBeInTheDocument();
+
+    const paragraphElement = screen.getByText("All fields are mandatory");
+    expect(paragraphElement).toBeInTheDocument();
+
+    // getByAltText with image element
+    const imgElement = screen.getByAltText("something cute")
+    expect(imgElement).toBeInTheDocument();
+
+    /*========================================================================*/
+    // NAME INPUT ELEMENT
+    // getByRole
+    const nameElement = screen.getByRole("textbox", { name: "Name" });
+    expect(nameElement).toBeInTheDocument();
+
+    // getByLabelText
+    const nameElement2 = screen.getByLabelText("Name", { selector: "input" });
+    expect(nameElement2).toBeInTheDocument();
+
+    // getByPlaceholderText
+    const nameElement3 = screen.getByPlaceholderText("Full name");
+    expect(nameElement3).toBeInTheDocument();
+
+    // getByDisplayValue()
+    const nameElement4 = screen.getByDisplayValue('Michael Padin');
+    expect(nameElement4).toBeInTheDocument();
+
+    //
+    /*========================================================================*/
+
+    /*========================================================================*/
+    // TEXTAREA ELEMENT
+    // getByRole
+    const bioElement = screen.getByRole("textbox", { name: "Bio" });
+    expect(bioElement).toBeInTheDocument();
+    /*========================================================================*/
+
+    /*========================================================================*/
+    // SELECT ELEMENT
+    // getByRole
+    const jobLocationElement = screen.getByRole("combobox");
+    expect(jobLocationElement).toBeInTheDocument();
+    /*========================================================================*/
+
+    /*========================================================================*/
+    // CHECK BOX
+    // getByRole
+    const termsElement = screen.getByRole("checkbox");
+    expect(termsElement).toBeInTheDocument();
+    // getByLabelText
+    const termsElement2 = screen.getByLabelText(
+      "I agree to the terms and conditions"
+    );
+    expect(termsElement2).toBeInTheDocument();
+    /*========================================================================*/
+
+    /*========================================================================*/
+    //button element
+    // getByRole
+    const submitButton = screen.getByRole("button");
+    expect(submitButton).toBeInTheDocument();
+    /*========================================================================*/
   });
 });
--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -1,31 +1,62 @@
 import React from "react";
 
 const Application = () => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log({ [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
-      </div>
-      <div>
-        <label htmlFor="job-location">Job location</label>
-        <select name="job" id="job-location">
-          <option value="">Select a country</option>
-          <option value="US">United States</option>
-          <option value="GB">United Kingdom</option>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-          <option value="CA">Canada</option>
-          <option value="IN">India</option>
-          <option value="AU">Australia</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="">
-          <input type="checkbox" id="terms" />I agree to the terms and
-          conditions
-        </label>
-      </div>
-      <button>Submit</button>
-    </form>
+    <>
+      <h1>Job Application Form</h1>
+      <h2>Section 1</h2>
+      <p>All fields are mandatory</p>
+      <img src= "https://source.unsplash.com/random/300x300" alt = "something cute"/>
+      <form onSubmit={handleSubmit}>
+        {/* NAME INPUT ELEMENT */}
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="jobName"
+            placeholder="Full name"
+            defaultValue={"Michael Padin"}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          {/* textarea */}
+          <div>
+            <label htmlFor="bio">Bio</label>
+            <textarea id="bio" name="bio"></textarea>
+          </div>
+
+          {/* Select element */}
+          <label htmlFor="job-location">Job Location</label>
+          <select name="job" id="job-location">
+            <option value="">Select a country</option>
+            <option value="US">United States</option>
+            <option value="GB">United Kingdom</option>
+            <option value="CA">Canada</option>
+            <option value="IN">India</option>
+            <option value="AU">Australia</option>
+          </select>
+        </div>
+
+        {/* Checkbox element */}
+        <div>
+          <label htmlFor="terms">
+            <input type="checkbox" id="terms" />I agree to the terms and
+            conditions
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </>
   );
 };
 
