cbad1c5c043662c7a16dede2cca63368b267c2af
Your Name <padinmichael201@gmail.com>
2022-10-04 21:13:37 +0800

    test: add user event test

--- src/components/Counter/Counter.test.tsx
+++ src/components/Counter/Counter.test.tsx
@@ -1,4 +1,5 @@
-import{screen, render, getByRole} from "@testing-library/react"
+import{screen, render} from "@testing-library/react"
+import user from "@testing-library/user-event";
 import Counter from './Counter'
 
 describe("Counter", () => {
@@ -19,5 +20,34 @@ describe("Counter", () => {
 
         const countElement = screen.getByRole('heading');
         expect(countElement).toHaveTextContent('0');
+    })
+    
+    it('renders a count of 1 after clicking the increment button', async () => {
+        user.setup();
+        render(<Counter/>);
+        
+        const incrementButton = screen.getByRole('button', {
+            name: 'Increment',
+        })
+        await user.click(incrementButton )
+        
+        const countElement = screen.getByRole('heading');
+        expect(countElement).toHaveTextContent('1');
+        
+    })
+    
+    
+    it('renders a count of 2 after clicking the increment button twice',  async() => {
+        user.setup();
+        render(<Counter/>);
+        
+        const incrementButton = screen.getByRole('button', {
+            name: 'Increment'
+        })
+
+        await user.dblClick(incrementButton);
+
+        const countElement = screen.getByRole('heading');
+        expect(countElement).toHaveTextContent('2');
     })
 })
