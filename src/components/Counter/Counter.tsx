e0e5d7d07c0de47a5af7964f76a1bf2191e97950
Your Name <padinmichael201@gmail.com>
2022-10-07 23:53:25 +0800

    test : add new input element

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 
 const Counter = () => {
   const [counter, setCounter] = useState(0);
-  
+  const [amount, setAmount] = useState(0);
+
   return (
     <div>
       <h1>{counter}</h1>
       <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <input type = "number" name = "amount" value = {amount}  onChange = {(e) => setAmount(parseInt(e.target.value))}/>
     </div>
   );
 };
